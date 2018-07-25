5be7820bbba534b682b13302df2752bcb668d71d
Wei Zhou <alsor.zhou@gmail.com>
2018-07-25 13:01:55 +0800

    Remove no use resources

--- languages/en.js
+++ languages/en.js
@@ -1,92 +1,68 @@
 if( currentLanguage == "en"){
 	app.controller("language", function ($scope) {
     	    $scope.language = {
-    	    // Menu
-	        "menu-item-1": 'PMD',
-	        "menu-item-2": 'Machine',
-	        "menu-item-3": 'Token',
-	        "menu-item-4": 'About',
-	        "menu-item-5": 'White Paper',
-	        "menu-item-white-paper": 'Pyramid_Whitepaper_EN_1.0.pdf',
-	        
-	        // ticker
-	        "ticker": 'PMD将在8月开展全球超级节点竞选，届时PMD运营方新加坡JIAHE基金将会对竞选节点开展空投奖励，空投总奖励约2亿PMD，空投奖励将用于全球市场推广，本次空投全部来自于基金会锁仓的3.5亿PMD。',
-
-	    	// HOME PAGE
-	    	// slide 1
-	        "slide1-title-1": 'Pyramid',
-	        "slide1-title-2": 'Pyramid',
-	        "slide1-p": 'New-generation file storage layer protocol',
-	        "slide1-right-p": 'Online countdown',
-	        // slide2
-	        "slide2-title-1": 'PYRAMID',
-	        "slide2-title-2": 'Ecosystem',
-	        "slide2-ecosystem-1": 'Pyramid PC',
-	        "slide2-ecosystem-2": 'Pyramid Chain',
-	        "slide2-ecosystem-3": 'Pyramid Coin',
-	        "slide2-ecosystem-text-1": 'Pyramid PC is a hardware host that based on content addressing technology point-to-point hypermedia protocol storage, Pyramid Chain public chain management of digital assets, customized reward system for users through smart contracts.',
-	        "slide2-ecosystem-text-2": 'Pyramid Chain is a de-intermediary public block-chain platform that serves network storage files. It is dedicated to building a block-chain network system in which information, interoperability, and value exchange in the field of network storage files are exchanged.',
-	        "slide2-ecosystem-text-3": 'PMD is a decentralized peer-to-peer payment network built to support the ecological construction of the platform， and a consensus-based mechanism that facilitates the creation of a new payment system and a fully digital currency.',
-	        // slide3
-	        "slide3-title-1": 'PYRAMID',
-	        "slide3-title-2": 'Ecological composition',
-	        "slide3-img": './img/EN.png',
-	        "slide3-p-1": 'PYRAMID is a global distributed IaaS ecosystem service platform that combines the latest blockchain technology and is specifically designed for the next generation of intelligent commodity circulation trading platforms. It solves problems in certification, security and interoperability in the supply chain. Helps protect the network while eliminating the need for centralized management.',
-	        // slide 4
-	        "slide4-title-1": 'PYRAMID',
-	        "slide4-title-2": 'News',
-	        "slide4-p": 'Media reports, press releases and more.',
-	        "slide4-menu-1": 'Pyramid establishes formal partnership with imToken',
-	        "slide4-menu-2": 'What is the Pyramid?',
-	        "slide4-menu-3": 'How Pyramid Brings You More Benefits',
-	        // slie5
-	        "slide5-title-1": 'PYRAMID',
-	        "slide5-title-2": 'Subscribe',
-	        "slide5-button": 'Subscribe',
-	        "slide5-p": 'Subscribe to us and get more information in time.',
-	        "slide5-placeholder": 'Please enter your email',
-	        // slie6
-	        "slide6-title-1": 'PYRAMID',
-	        "slide6-title-2": 'Contact us',
-	        "slide6-p": 'Questions, advice, cooperation, etc., look forward to your letter to us.',
-
-	        // MACHINE PAGE
-	        "machine-title-1": 'PYRAMID',
-	        "machine-title-2": 'Technology',
-	        "machine-p-1": 'Pyramid will combine the physical mining machine technology, and the token PMCC in the Pyramid ecosystem will be acquired by the user through cloud mining to build a rich and ',
-	        "machine-p-2": 'available distributed node and maintain the stability of the network.',
-
-	        // ABOUT PAGE
-	        "about-title": 'About',
-	        "about-img": './img/ORG_EN.jpg',
-	        "about-p-1": 'Pyramid is affiliated with the Singapore Golden Harvest Foundation (referred as the “Foundation”) and is committed to promoting the development and construction of Pyramid Ecology and the promotion of governance transparency. And it’s to promote the safe and harmonious development of the open source ecological society. ',
-	        "about-p-2": 'The foundation will help to manage the sustainability of open source community projects, the effectiveness of management, and the safety of raised funds through the development of a good governance structure. ',
-	        "about-p-3": 'The foundation consists of an ecological center, a technology development center, a marketing center, and a daily management center.',
-	        "about-p-4": `Pyramid's decision-making committee is responsible for the management and decision of major issues, including the appointment or dismissal of executive officers and heads of various centers, and the formulation of important decisions. The members of the policy committee are appointed for a term of three years and can be reappointed. The committee has one chairman who will be decided by the committee members.`,
-	        "about-p-5": 'The members of the decision-making committee will be elected by the founding team and investors of Pyramid.',
-	        "about-p-6": 'Technology Development Center: responsible for the development, testing, on-line, and audit of the underlying technology. Members of the technical center communicate with token holders in the community and hold technical conferences from time to time.',
-	        "about-p-7": 'Ecological center: responsible for exploring the feasibility of Pyramid and landing industry application scenarios to realize the commercial landing of Pyramid. Focus on areas of exploration: cross-border e-commerce, second-handed car trading, supply chain finance, big data, cross-border transactions.',
-	        "about-p-8": 'Marketing Center: responsible for the promotion and promotion of technology, products, communities, promotion of source projects.',
-	        "about-p-9": 'Daily management center:responsible for matters including finance, legal affairs, personnel, and administration.',
-
-	        // TOKEN PAGE
-	        "token-title-1": 'Distribution',
-	        "token-p-1": '',
-	        "token-p-2": 'The project team will invest 800 bitcoins in the early stages for system development and community operations. The total planned circulation is 84 million PMD.',
-	        "token-p-3": 'Here is the specific allocation plan:',
-
-	        "token-progress-title-1": 'The production of mining',
-	        "token-progress-p-1": `45% of the total circulation will be delivered to the user's market through the Pyramid PC mining mechanism, and 450 million PMD will be rewarded through mining.`,
-
-	        "token-progress-title-2": 'Project Foundation Reserve',
-	        "token-progress-p-2": `35% of the total circulation is used as foundation reserve funds for technology development, community operations, business cooperation, promotion costs, and project ecological improvement in the follow-up project of Pyramid Ecology. The use of the funds requires resolution of foundation and public announcement in advance.`,
-
-	        "token-progress-title-3": 'Initial Team',
-	        "token-progress-p-3": '12% of total circulation is used to attract early investors. Early investors will contribute to team building and platform operations.',
-
-	        "token-progress-title-4": 'Technology Development',
-	        "token-progress-p-4": '8% of the total circulation is awarded to the initial team and development team.',
-
+ 	    	// Menu
+			 "menu-item-1": 'Home',
+			 "menu-item-3": 'Token',
+			 "menu-item-4": 'About',
+			 "menu-item-5": 'White Paper',
+			 "menu-item-white-paper": 'LP_Whitepaper_CN_1.0.pdf',
+			 
+			 // HOME PAGE
+			 // slide 1
+			 "slide1-title-1": '无忧链',
+			 "slide1-title-2": 'Labor sharing public welfare transparent service platform',
+			 "slide1-p": 'The LP chain token platform is based on the establishment of blockchain technology, with smart contracts as payment technology, giving full play to the decentralization of blockchain and the fairness and transparency of information. It is the platform that all information can be shared and trusted, providing users with high Quality workforce services.',
+			 "slide1-right-p": 'Countdown',
+			 // slide2
+			 "slide2-title-1": '无忧链',
+			 "slide2-title-2": 'Ecosystem',
+			 "slide2-ecosystem-1": 'LP Ecosystem',
+			 "slide2-ecosystem-2": 'LP Chain',
+			 "slide2-ecosystem-3": 'LP Coin',
+			 "slide2-ecosystem-text-1": 'LP Ecosystem opens the era of globalized shared labor service digitalization, realizing the benefits of no middlemen exploiting workers, and the platform does not draw any commission.',
+			 "slide2-ecosystem-text-2": `LP Chain is the world's first labor-sharing public service transparent service platform. It is a new ecosystem based on blockchain technology development, and is committed to building a global shared and transparent labor service platform.',
+			 "slide2-ecosystem-text-3": 'LP Token By averaging the underlying idle workforce, the average economic benefit per hour that each Token represents can be created.',
+			 // slide3
+			 "slide3-title-1": '无忧链',
+			 "slide3-title-2": 'Ecosystem Architecture',
+			 "slide3-img": './img/diagramma.png',
+			 "slide3-p-1": 'The LP chain running system adopts the module componentized underlying architecture. When users build blockchain applications or sub-chains, most of the components are designed to be referenced and pluggable. For example, the working component supports POW. POS, DPOS, PBFT, etc.',
+			 // slide 4
+			 "slide4-title-1": '无忧链',
+			 "slide4-title-2": 'News',
+			 "slide4-p": 'Media reports, press releases, and more!',
+			 "slide4-menu-1": '',
+			 "slide4-menu-2": '',
+			 "slide4-menu-3": '',
+			 // slie5
+			 "slide5-title-1": '无忧链',
+			 "slide5-title-2": 'Subscribe',
+			 "slide5-button": 'Subscribe',
+			 "slide5-p": 'Subscribe and get more information in time!',
+			 "slide5-placeholder": 'Please enter your email address',
+			 // slie6
+			 "slide6-title-1": '无忧链',
+			 "slide6-title-2": 'Contact Us',
+			 "slide6-p": 'Questions, consultation, cooperation, etc., we are looking forward to hearing from you',
+  
+			 // TOKEN PAGE
+			 "token-title-1": 'Token',
+			 "token-p-1": 'Project',
+			 "token-p-2": 'The planned total circulation is 1 billion LP Tokens.',
+			 "token-p-3": 'Specific allocation plan:',
+ 
+			 "token-progress-title-1": 'Pre sale',
+			 "token-progress-p-1": '39%',
+ 
+			 "token-progress-title-2": 'Team Operation',
+			 "token-progress-p-2": '30%',
+ 
+			 "token-progress-title-3": 'Team Motivation',
+			 "token-progress-p-3": '20%',
+ 
+			 "token-progress-title-4": 'Technical application development',
+			 "token-progress-p-4": '11%',
 	    };
 	});
 }
