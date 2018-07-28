7f6376720b0395508d594bf9be18ad7ede9fe235
Wei Zhou <alsor.zhou@gmail.com>
2018-07-28 22:37:54 +0800

    Update translation

--- languages/en.js
+++ languages/en.js
@@ -10,12 +10,12 @@ if( currentLanguage == "en"){
 			 
 			 // HOME PAGE
 			 // slide 1
-			 "slide1-title-1": '无忧链',
+			 "slide1-title-1": 'Labor Share Network',
 			 "slide1-title-2": 'Labor sharing public welfare transparent service platform',
 			 "slide1-p": 'The LP chain token platform is based on the establishment of blockchain technology, with smart contracts as payment technology, giving full play to the decentralization of blockchain and the fairness and transparency of information. It is the platform that all information can be shared and trusted, providing users with high Quality workforce services.',
 			 "slide1-right-p": 'Countdown',
 			 // slide2
-			 "slide2-title-1": '无忧链',
+			 "slide2-title-1": 'Labor Share Network',
 			 "slide2-title-2": 'Ecosystem',
 			 "slide2-ecosystem-1": 'LP Ecosystem',
 			 "slide2-ecosystem-2": 'LP Chain',
@@ -24,25 +24,25 @@ if( currentLanguage == "en"){
 			 "slide2-ecosystem-text-2": 'LP Chain is the world\'s first labor-sharing public service transparent service platform. It is a new ecosystem based on blockchain technology development, and is committed to building a global shared and transparent labor service platform.',
 			 "slide2-ecosystem-text-3": 'LP Token By averaging the underlying idle workforce, the average economic benefit per hour that each Token represents can be created.',
 			 // slide3
-			 "slide3-title-1": '无忧链',
+			 "slide3-title-1": 'Labor Share Network',
 			 "slide3-title-2": 'Ecosystem Architecture',
 			 "slide3-img": './img/diagramma.png',
 			 "slide3-p-1": 'The LP chain running system adopts the module componentized underlying architecture. When users build blockchain applications or sub-chains, most of the components are designed to be referenced and pluggable. For example, the working component supports POW. POS, DPOS, PBFT, etc.',
 			 // slide 4
-			 "slide4-title-1": '无忧链',
+			 "slide4-title-1": 'Labor Share Network',
 			 "slide4-title-2": 'News',
 			 "slide4-p": 'Media reports, press releases, and more!',
 			 "slide4-menu-1": '',
 			 "slide4-menu-2": '',
 			 "slide4-menu-3": '',
 			 // slie5
-			 "slide5-title-1": '无忧链',
+			 "slide5-title-1": 'Labor Share Network',
 			 "slide5-title-2": 'Subscribe',
 			 "slide5-button": 'Subscribe',
 			 "slide5-p": 'Subscribe and get more information in time!',
 			 "slide5-placeholder": 'Please enter your email address',
 			 // slie6
-			 "slide6-title-1": '无忧链',
+			 "slide6-title-1": 'Labor Share Network',
 			 "slide6-title-2": 'Contact Us',
 			 "slide6-p": 'Questions, consultation, cooperation, etc., we are looking forward to hearing from you',
   
